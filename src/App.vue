04275e4c9ae6e2c3b2434b4075cd4dc6b3efd849
Moiseiev <promotion.x.developer1@gmail.com>
2025-01-29 10:20:38 +0200

    Improve layout and fix mobile display issues

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref, computed } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import { useDisplay } from 'vuetify';
 
-const drawer = ref(true);
+const drawer = ref(null);
 const searchQuery = ref('');
 const router = useRouter();
+const route = useRoute();
+const { mobile, mdAndUp } = useDisplay();
 
 const menuItems = [
 	{ title: 'Головна', icon: 'mdi-home', to: '/' },
@@ -19,53 +22,81 @@ const menuItems = [
 	{ title: 'Словники', icon: 'mdi-dictionary', to: '/dictionaries' },
 ];
 
+const isCurrentRoute = computed(() => (path) => {
+	return route.path === path;
+});
+
 const search = () => {
 	if (searchQuery.value) {
 		router.push({ name: 'search', query: { q: searchQuery.value } });
+		searchQuery.value = '';
+		if (mobile.value) drawer.value = false;
 	}
 };
 </script>
 
 <template>
 	<v-app>
-		<v-navigation-drawer v-model="drawer" permanent>
-			<v-list>
-				<v-list-item v-for="(item, i) in menuItems" :key="i" :to="item.to" :prepend-icon="item.icon" :title="item.title" />
+		<v-navigation-drawer v-model="drawer" :permanent="mdAndUp" :temporary="!mdAndUp" :location="mdAndUp ? 'left' : 'start'" width="300">
+			<v-list-item prepend-icon="mdi-language-python" title="Python Tutorial" nav>
+				<template v-slot:append>
+					<v-btn v-if="!mdAndUp" variant="text" icon="mdi-close" @click="drawer = false"></v-btn>
+				</template>
+			</v-list-item>
+
+			<v-divider></v-divider>
+
+			<v-list nav density="compact">
+				<v-list-item
+					v-for="item in menuItems"
+					:key="item.title"
+					:to="item.to"
+					:prepend-icon="item.icon"
+					:title="item.title"
+					:active="isCurrentRoute(item.to)"
+					link
+					@click="mobile ? (drawer = false) : null"
+					class="rounded-lg mb-1"
+				>
+					<template v-slot:append v-if="isCurrentRoute(item.to)">
+						<v-icon color="primary" icon="mdi-check-circle"></v-icon>
+					</template>
+				</v-list-item>
 			</v-list>
 		</v-navigation-drawer>
 
-		<v-app-bar>
-			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-			<v-toolbar-title>Python підручник</v-toolbar-title>
+		<v-app-bar elevation="1">
+			<v-app-bar-nav-icon v-if="!mdAndUp" @click="drawer = !drawer"></v-app-bar-nav-icon>
+			<v-app-bar-title class="text-primary font-weight-bold">
+				<v-icon icon="mdi-language-python" color="primary" class="mr-2"></v-icon>
+				<span :class="mobile ? 'text-subtitle-1' : 'text-h6'">Python підручник</span>
+			</v-app-bar-title>
 			<v-spacer></v-spacer>
-			<v-text-field v-model="searchQuery" append-icon="mdi-magnify" label="Пошук" single-line hide-details @keyup.enter="search"></v-text-field>
+
+			<v-text-field
+				v-model="searchQuery"
+				:class="mobile ? 'mx-2 w-120' : 'mx-4 w-300'"
+				prepend-inner-icon="mdi-magnify"
+				label="Пошук..."
+				single-line
+				hide-details
+				density="compact"
+				variant="solo-filled"
+				rounded
+				@keyup.enter="search"
+			>
+				<template v-slot:append>
+					<v-fade-transition leave-absolute>
+						<v-icon v-if="searchQuery" color="primary" @click="searchQuery = ''" class="cursor-pointer"> mdi-close-circle </v-icon>
+					</v-fade-transition>
+				</template>
+			</v-text-field>
 		</v-app-bar>
 
-		<v-main>
-			<v-container fluid>
+		<v-main :class="mdAndUp ? 'ml-300' : ''">
+			<v-container fluid class="pa-4">
 				<router-view></router-view>
 			</v-container>
 		</v-main>
 	</v-app>
 </template>
-
-<style>
-.v-navigation-drawer {
-	width: 300px !important;
-	position: fixed;
-	top: 0;
-	left: 0;
-	height: 100%;
-	z-index: 100;
-}
-
-.v-main {
-	padding-left: 300px;
-}
-
-@media (max-width: 960px) {
-	.v-main {
-		padding-left: 0;
-	}
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,27 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import { createVuetify } from 'vuetify';
-import '@mdi/font/css/materialdesignicons.css';
+
+// Vuetify
 import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
 const vuetify = createVuetify({
+	components,
+	directives,
 	theme: {
 		defaultTheme: 'light',
+		themes: {
+			light: {
+				colors: {
+					primary: '#1867C0',
+					secondary: '#5CBBF6',
+				},
+			},
+		},
 	},
 });
 
