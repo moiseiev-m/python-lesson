eabef2aeba0513e4ea5554df2fd8ede68a0ac416
Moiseiev <promotion.x.developer1@gmail.com>
2025-01-30 09:41:59 +0200

    Update favicon and title

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: './',
+	base: process.env.NODE_ENV === 'production' ? './' : '/',
 	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {
